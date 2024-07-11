dcd160b2c86d2ba59bafc5eb921c55b9113ebb90
Sean O'Connor <so@seano.dev>
2024-07-11 16:16:37 -0600

    Case-insensitive querying

--- backend/src/main/kotlin/dev/seano/catgram_backend/feature/pet/PetRepository.kt
+++ backend/src/main/kotlin/dev/seano/catgram_backend/feature/pet/PetRepository.kt
@@ -7,9 +7,9 @@ import org.springframework.stereotype.Repository
 @Repository
 interface PetRepository : CrudRepository<Pet, Int> {
 
-	@Query("SELECT p FROM Pet p JOIN p.user u WHERE u.user.username = :username")
+	@Query("SELECT p FROM Pet p JOIN p.user u WHERE LOWER(u.user.username) = LOWER(:username)")
 	fun findAllByUsername(username: String): List<Pet>
 
-	@Query("SELECT p FROM Pet p JOIN p.user u WHERE u.user.username = :username AND p.name = :petName")
+	@Query("SELECT p FROM Pet p JOIN p.user u WHERE LOWER(u.user.username) = LOWER(:username) AND LOWER(p.name) = LOWER(:petName)")
 	fun findByNameAndUsername(petName: String, username: String): List<Pet>
 }
